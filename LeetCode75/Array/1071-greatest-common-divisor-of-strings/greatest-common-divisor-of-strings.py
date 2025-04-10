b1224ce3f68c81b57bcd6bb529d6adcec9fe7b49
Aria <80862548+asab77@users.noreply.github.com>
2025-04-10 11:46:06 -0700

    Create greatest-common-divisor-of-strings.py

---
+++ LeetCode75/Array/1071-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,12 @@
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if (str1 + str2 == str2 + str1):
+            gcd_len = gcd(len(str1), len(str2))
+            return str2[:gcd_len]
+        
+        else:
+            return ""
+
+        
